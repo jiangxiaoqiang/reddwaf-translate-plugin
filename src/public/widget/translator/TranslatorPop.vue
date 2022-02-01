bca45e37b7e2c8c1acb74871e521eb3eb8aad76e
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-02-01 13:10:23 +0800

    Create TranslatorPop.vue

---
+++ src/public/widget/translator/TranslatorPop.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>I am a pop</div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+<style scoped>
+
+
+</style>
